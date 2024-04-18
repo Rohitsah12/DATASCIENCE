9e8c5871d6e54a14ca35e6b65d05a3e184f7abae
Rohit Kumar Sah <148933953+Rohitsah12@users.noreply.github.com>
2024-04-18 11:24:54 +0530

    update

--- practice.py
+++ practice.py
@@ -265,6 +265,59 @@ def adjacent_value_sum():
         sum+=x
         new_lis.append(x)
     print(new_lis)
-adjacent_value_sum()
+
+def prefix_sum():
+    lis=list(map(int,input().split()))
+    prefix_array=[]
+    prefix_array.append(lis[0])
+    for i in range(1,len(lis)):
+        x=prefix_array[i-1]+lis[i]
+        prefix_array.append(x)
+    print(prefix_array)
+
+def check_if_array_represent_fibonacci_sequence_or_not():
+    lis=list(map(int,input().split()))
+    x=len(lis)
+    fibonacci_array=[1,1]
+    for i in range(2,len(lis)):
+        fibonacci_array.append(fibonacci_array[i-2]+fibonacci_array[i-1])
+
+    if(fibonacci_array==lis):
+        print("Yes")
+    else:
+        print("NO")
+
+def sum_of_each_row_in_2d_array():
+    n = int(input("Enter the number of rows:")) 
+    m = int(input("Enter the number of columns:")) 
+  
+    matrix = [] 
+
+    print("Enter values in matrix :") 
+
+# For user input 
+    for i in range(n):
+        data =[] 
+        for j in range(m):
+            data.append(int(input())) 
+        matrix.append(data) 
+
+# For printing the matrix 
+    for i in range(n): 
+        for j in range(m): 
+            print(matrix[i][j], end = " ") 
+        print()
+
+
+# For printing row wise sum 
+    for i in range(n):
+        sum = 0
+        for j in range(m): 
+            sum = sum + matrix[i][j]
+        print('Sum of row',i+1,':',sum)
+
+sum_of_each_row_in_2d_array()
+
+    
 
 
